3f65718badcbeb887e430a28d417db28e62f0ee3
Tianyu <ztyame@gmail.com>
2021-03-04 00:49:12 -0500

    build

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Header from "./components/Header";
 // import "./App.css";
 import PathfindingVisualizer from "./PathfindingVisualizer/PathfindingVisualizer";
 
 function App() {
   return (
     <div className="App body-style">
-      <Header />
       <PathfindingVisualizer />
     </div>
   );
--- src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import PathfindingVisualizer from "../PathfindingVisualizer/PathfindingVisualizer";
-import "./Header.css";
-
-function Header() {
-  return <header></header>;
-}
-
-export default Header;
